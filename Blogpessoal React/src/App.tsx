7f1d9b22130f3f32b28223c66a252e256b2d2a74
unknown <guilherme.neto2001@outlook.com>
2023-10-11 11:05:35 -0300

    atualizações finais blogpessoal

--- Blogpessoal React/src/App.tsx
+++ Blogpessoal React/src/App.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
-import './App.css';
 
+import './App.css';
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from 'react-toastify';
 import Navbar from './components/navBar/NavBar';
 import Footer from './components/footer/Footer';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
@@ -11,14 +12,19 @@ import { AuthProvider } from './contexts/AuthContext';
 import ListaTemas from './components/temas/listaTemas/ListaTemas';
 import FormularioTema from './components/temas/formularioTema/FormularioTema';
 import DeletarTema from './components/temas/deletarTema/DeletarTema';
-
+import ListaPostagens from './components/postagens/listaPostagens/ListaPostagens';
+import FormularioPostagem from './components/postagens/formularioPostagem/FormularioPostagem';
+import DeletarPostagem from './components/postagens/deletarPostagem/DeletarPostagem';
+import Perfil from './paginas/perfil/Perfil';
 
 function App() {
+  
   return (
     <>
-    <AuthProvider>
+      <AuthProvider>
+      <ToastContainer />
         <BrowserRouter>
-          <Navbar />
+        <Navbar />
           <div className='min-h-[80vh]'>
             <Routes>
               <Route path="/" element={<Login />} />
@@ -29,12 +35,18 @@ function App() {
               <Route path="/cadastroTema" element={<FormularioTema />} />
               <Route path="/editarTema/:id" element={<FormularioTema />} />
               <Route path="/deletarTema/:id" element={<DeletarTema />} />
+              <Route path="/postagens" element={<ListaPostagens />} />
+              <Route path="/cadastroPostagem" element={<FormularioPostagem />} />
+              <Route path="/editarPostagem/:id" element={<FormularioPostagem />} />
+              <Route path="/deletarPostagem/:id" element={<DeletarPostagem />} />
+              <Route path="/perfil" element={<Perfil />} />
             </Routes>
           </div>
           <Footer />
         </BrowserRouter>
-        </AuthProvider>
+      </AuthProvider>
     </>
   );
 }
+
 export default App;
--- Blogpessoal React/src/components/temas/cardTemas/CardTemas.tsx
+++ Blogpessoal React/src/components/temas/cardTemas/CardTemas.tsx
@@ -1,14 +1,14 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import {Link} from 'react-router-dom'
 import Tema from '../../../models/Tema'
 
-interface CardTemaProps {
+interface CardTemasProps{
   tema: Tema
 }
 
-function CardTemas({tema}: CardTemaProps) {
-  return (
-    <div className='border flex flex-col rounded-2xl overflow-hidden justify-between'>
+function CardTemas({tema}: CardTemasProps) {
+    return (
+      <div className='border flex flex-col rounded-2xl overflow-hidden justify-between'>
       <header className='py-2 px-6 bg-indigo-800 text-white font-bold text-2xl'>Tema</header>
       <p className='p-8 text-3xl bg-slate-200 h-full'>{tema.descricao}</p>
       <div className="flex">
@@ -22,5 +22,5 @@ function CardTemas({tema}: CardTemaProps) {
     </div>
   )
 }
-
-export default CardTemas
+  
+  export default CardTemas
